946cfc774aa4779d05caf849717af916da9ca412
Joao Arthur Pereira de Souza <JT0075306@mateus.dc>
2025-06-13 08:23:10 -0300

    posting updates

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,9 +1,23 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
+    def __init__(self, nome, categoria):
+        self.nome = nome.tittle()
+        self.categoria = categoria
+        self._ativo = False   # O _ antes da variavel significa que o programa não vai deixar que o usuário altere o valor dele (O usuário não vai acessar ele diretamente, somente através do metódo @property)
+        Restaurante.restaurantes.append(self)
 
-restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def list_restaurants():
+        for restaurant in Restaurante.restaurantes:
+            print(f'{restaurant.nome} | {restaurant.categoria} | {restaurant.ativo}')
+     
+    @property
+    def ativo(self):
+        return 'Ativo' if self._ativo else 'Desativado'
+    
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiano')
 
-restaurantes = [restaurante_pizza, restaurante_praca]
+Restaurante.list_restaurants()
